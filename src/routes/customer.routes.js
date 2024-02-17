0111aab4da6a9c411af175eee377a4ab165c42d2
pankaj-kb <pankajb2601@gmail.com>
2024-02-17 14:43:27 +0530

    added routes for controller functions

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { registerCustomer, loginCustomer, logoutCustomer } from "../controllers/customer.controller.js";
+import { registerCustomer, loginCustomer, logoutCustomer, getTransactions } from "../controllers/customer.controller.js";
 
 const router = Router();
 
 router.route('/register').post(registerCustomer)
 router.route('/login').post(loginCustomer)
 router.route('/logout').post(verifyJWT, logoutCustomer)
+router.route('/transactions/:userId').get(getTransactions)
 
 export default router;
