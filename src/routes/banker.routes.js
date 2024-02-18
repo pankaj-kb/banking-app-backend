836c7fc2d3bd76555eb6d251062619c7c367a096
pankaj-kb <pankajb2601@gmail.com>
2024-02-19 00:25:05 +0530

    fixed routing for customer Username instead of customer id

--- src/routes/banker.routes.js
+++ src/routes/banker.routes.js
@@ -14,6 +14,6 @@ router.route('/register').post(registerBanker)
 router.route('/login').post(loginBanker)
 router.route('/logout').post(verifyJWT, logoutBanker)
 router.route('/customers').get(verifyJWT, getAllCustomers)
-router.route('/customer/:customerId').get(verifyJWT, getCustomerInfo)
+router.route('/customer/:customerusername').get(verifyJWT, getCustomerInfo)
 
 export default router;
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { registerCustomer, loginCustomer, logoutCustomer, getTransactions } from "../controllers/customer.controller.js";
+import { registerCustomer, loginCustomer, logoutCustomer, getTransactions, getCurrentUser } from "../controllers/customer.controller.js";
 
 const router = Router();
 
 router.route('/register').post(registerCustomer)
 router.route('/login').post(loginCustomer)
 router.route('/logout').post(verifyJWT, logoutCustomer)
-router.route('/transactions/:userId').get(getTransactions)
+router.route('/transactions/:userId').get(verifyJWT, getTransactions)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
 
 export default router;
