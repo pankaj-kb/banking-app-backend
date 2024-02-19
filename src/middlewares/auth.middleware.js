0d74525fb06245e83a95fb18c3caf5796a211153
pankaj-kb <pankajb2601@gmail.com>
2024-02-19 15:03:11 +0530

    removing setCookie from Customer

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
-        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        const token = await req.header("Authorization")?.replace("Bearer ", "");
         console.log("token at 10: ",token)
 
         if (!token) {
