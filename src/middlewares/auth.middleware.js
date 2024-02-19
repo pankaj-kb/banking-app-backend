ec56df54b67beb30fec85672c3525cdfe5cf876b
pankaj-kb <pankajb2601@gmail.com>
2024-02-19 11:38:46 +0530

    adding Banker login persistance and logginng error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,29 +5,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
-            throw new APIError(401, "Unauthorized request")
+            throw new APIError(401, "Unauthorized request");
         }
 
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        let user = await Customer.findById(decodedToken?._id).select("-password -refreshToken")
+        const findUserById = async (userId) => {
+            return await Customer.findById(userId).select("-password -refreshToken") || await Banker.findById(userId).select("-password -refreshToken");
+        };
+
+        const user = await findUserById(decodedToken?._id);
 
         if (!user) {
-            user = await Banker.findById(decodedToken?._id).select("-password -refreshToken")
-        }
-
-        if (!user) {
-            throw new APIError(401, "Authentication Error.")
+            throw new APIError(401, "Authentication Error.");
         }
 
         req.user = user;
-        next()
+        next();
     } catch (error) {
-        throw new APIError(401, error?.message || "Invalid Access token")
+        console.error('Authentication error:', error);
+        throw new APIError(401, error?.message || "Invalid Access token");
     }
-})
+});
